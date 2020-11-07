7221e78c3dc5d75c59a6ce752715379f8ec53897
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-11-07 15:49:20 -0600

    compile using ts-node

--- src/server/routes/globalRouter.ts
+++ src/server/routes/globalRouter.ts
@@ -1,5 +1,5 @@
-import express from 'express';
-import path from 'path';
+import * as express from 'express';
+import * as path from 'path';
 //import  { webpackDevDependencies } from '../webpackDevDependencies';
 //import webpack from "webpack";
 
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,14 +1,11 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import path from 'path';
-import url from 'url';
+import * as express from "express";
+import * as bodyParser from 'body-parser';
 
 import globalRouter from './routes/globalRouter';
 //import { webpackDevDependencies } from './webpackDevDependencies';
 import { initBot } from './bot';
-import { createRequest } from './utils/requestGenerator';
 
-const app = express();
+const app = express.application;
 const PORT = process.env.PORT || 2503;
 
 let server = require('http').Server(app);
@@ -26,36 +23,11 @@ const initServer = async () => {
   if (process.env.NODE_ENV === 'production') {
     console.log('Production proxy: (http to https)');
 
-    // Uncoment when uploading to a secure domain....  (https)
-
-    /*
-
-		app.enable('trust proxy');
-	
-		app.use((req, res, next) => {
-			if (req.secure) {
-				next();
-			} else if (req.method === 'GET' || req.method === 'HEAD') {
-				res.redirect(url.format({
-					protocol: 'https',
-					host: req.get('host'),
-					pathname: req.originalUrl,
-				}));
-			} else {
-				res.status(400).json({
-					error: 'invalid_request',
-					error_description: 'Please only use https for this domain',
-				});
-			}
-		});
-
-		*/
   } else if (process.env.NODE_ENV === 'development') {
     //app.use(webpackDevDependencies.middleware);
   }
 
   initBot();
-  createRequest();
   app.use('/', globalRouter);
 
   server.listen(PORT, () => {
