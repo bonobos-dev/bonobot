d997872642a56b2ef91b64115b58d4171aef2cc1
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-11-14 18:51:51 -0600

    Fix the Cleanroles file

--- src/server/commands/cleanroles.ts
+++ src/server/commands/cleanroles.ts
@@ -1,9 +1,9 @@
 import * as Discord from 'discord.js';
 
-import { MigBotCommand } from '../botApi';
+import CommandInterface from '../interfaces/CommandInterface';
 import { validateCommandRestrictions } from '../utils/botValidation';
 
-export default class Cleanroles implements MigBotCommand {
+export default class Cleanroles implements CommandInterface {
   private readonly _command = 'cleanroles';
 
   private currentEmbedMessage: Discord.Message;
@@ -31,69 +31,5 @@ export default class Cleanroles implements MigBotCommand {
       return;
     }
     console.log('clean roles called');
-
-    /*
-        function getRoles( rolesList: Discord.RoleManager | Discord.GuildMemberRoleManager ){
-            let rolesForServer:any = [];
-
-            rolesList.cache.forEach( role => {
-                let roleFound = {
-                    roleName: role.name,
-                    //roleId: role.id,
-                    //rolePermissions:JSON.stringify(role.permissions.toArray())
-                }
-    
-                //rolesForServer.push(roleFound);
-                rolesForServer.push(role.name);
-    
-            });
-
-            return rolesForServer;
-
-        }
-      
-   
-        var members = await msgObject.guild.members.fetch()
-
-        let membersFromServer:any = []
-
-        members.forEach(member => {
-
-            if(!member.roles.cache.has('684255900323938324')){
-                return;
-            }
-
-            let memberFound = {
-                //id: member.id,
-                username: member.user.username,
-                roles:JSON.stringify( getRoles(member.roles))
-            }
-
-            if(member.roles.cache.has('684255900323938324') && member.roles.cache.has('710650474877157397') ){
-                console.log(`(Eliminar 1)  chimp  + bonobo  ${JSON.stringify( getRoles(member.roles))}`);
-                //member.roles.remove(['742860718650425395','684255900323938324']);
-                //member.roles.remove('684255900323938324');
-
-                return;
-            }
-
-            /*
-            else if(!member.roles.cache.has('742860718650425395') && member.roles.cache.has('710650474877157397') ){
-                console.log(`(Eliminar 0) Bonobo  ${JSON.stringify( getRoles(member.roles))}`);
-                //member.roles.remove('742860718650425395');
-                return;
-            }
-
-          
-            
-            
-            
-            membersFromServer.push(memberFound)
-        })
-
-        console.log(membersFromServer)
-
-        */
   }
 }
-
