212db788efaa6ceb9e90b901b69f1cd6d7488167
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-12-01 16:40:36 -0600

    refactor to server.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,44 +1,33 @@
 import dotenv from 'dotenv';
-dotenv.config({path: `../../.env` });
+dotenv.config({ path: `../../.env` });
 dotenv.config();
 
 import express from 'express';
 import bodyParser from 'body-parser';
 import globalRouter from './routes/globalRouter';
-import Bonobot  from './Bonobot';
+import Bonobot from './Bonobot';
 import { createRequest } from './utils/bonobotsHerokuHack';
-
-
 
 const app = express();
 const PORT = process.env.PORT || 2503;
 
-
-
-console.log(
+console.info(
   'La aplicación está corriendo en el entorno: <<< ' +
     process.env.NODE_ENV +
     ' >>>'
 );
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+const bot = new Bonobot();
+bot.start();
+bot.apply();
 
-const initServer = () => {
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
+createRequest();
 
-  const bot = new Bonobot();
-  bot.start();
-  bot.apply();
-  
-  createRequest();
+app.use('/', globalRouter);
 
-  app.use('/', globalRouter);
-
-
-  app.listen(PORT, () => {
-    console.log(`El bonobot está corriendo en: <<< port ${PORT} >>> `);
-  });
-};
-
-initServer();
+app.listen(PORT, () => {
+  console.log(`El bonobot está corriendo en: <<< port ${PORT} >>> `);
+});
