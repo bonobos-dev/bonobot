d4d8a411e7a81fc6e634a7d494ca263d091db0b5
Salvador Hernandez Lopez <chavatloz@gmail.com>
2020-12-02 01:48:57 -0600

    refactor Bonobot.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -11,23 +11,19 @@ import { createRequest } from './utils/bonobotsHerokuHack';
 const app = express();
 const PORT = process.env.PORT || 2503;
 
-console.info(
-  'La aplicación está corriendo en el entorno: <<< ' +
-    process.env.NODE_ENV +
-    ' >>>'
-);
+console.info('The app is on : <<< ' + process.env.NODE_ENV + ' >>>');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const bot = new Bonobot();
 bot.start();
-bot.apply();
+bot.run();
 
 createRequest();
 
 app.use('/', globalRouter);
 
 app.listen(PORT, () => {
-  console.log(`El bonobot está corriendo en: <<< port ${PORT} >>> `);
+  console.log(`The bonobot is run on: <<< port ${PORT} >>> `);
 });
