64731abd252e7a265f0387fb1431301e4dc041f8
Miguel Rebolledo <mig_drp@hotmail.com>
2021-01-15 03:20:04 -0600

    Added express routers for the REST API endpoints of each entity.

--- src/server/routes/globalRouter.ts
+++ src/server/routes/globalRouter.ts
@@ -1,17 +1,26 @@
 import express from 'express';
 import path from 'path';
+import { authenticationMiddleware } from '../utils';
+
+import { TemaryRouter, CategoryRouter, GuildRouter, RoleRouter, RuleRouter, CommandRouter, AdRouter } from './';
 
 const router = express.Router();
 
-
-
 router.use('/img', express.static(path.join(__dirname, `../assets/img`)));
 
+router.use(authenticationMiddleware);
+router.use('/api', TemaryRouter, CategoryRouter, RoleRouter, RuleRouter, CommandRouter, AdRouter, GuildRouter);
 
-router.get('/', (req, res)=>{
+router.get('/', (req, res) => {
   res.send('Bienvenido al bonobot');
 });
 
+router.get('/redirected', (req, res) => {
+  console.log('Params..', req.params);
+  console.log('Body..', req.body);
+  console.log('Query..', req.query);
+  res.send('Redirected from discord');
+});
 
 router.get('/start_requests', (_req, res) => {
   res.send('Hola hermoso bot, hacemos un buen trabajo, yo soy el bonobot');
---
+++ src/server/routes/index.ts
@@ -0,0 +1,10 @@
+import TemaryRouter from './TemaryRouter';
+import CategoryRouter from './CategoryRouter';
+import GuildRouter from './GuildRouter';
+import RoleRouter from './RoleRouter';
+import RuleRouter from './RuleRouter';
+import CommandRouter from './CommandRouter';
+import AdRouter from './AdRouter';
+import GlobalRouter from './globalRouter';
+
+export { TemaryRouter, CategoryRouter, GlobalRouter, RoleRouter, RuleRouter, CommandRouter, AdRouter, GuildRouter };
