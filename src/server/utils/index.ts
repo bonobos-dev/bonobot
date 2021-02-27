cdef4142ff34400c75b2c1de469a6f8ca1deb082
Miguel Rebolledo <mig_drp@hotmail.com>
2021-02-27 10:21:38 -0600

    db changes

--- src/server/utils/index.ts
+++ src/server/utils/index.ts
@@ -2,10 +2,9 @@ import { startHerokuHackRequest } from './bonobotHerokuHack';
 import { isBot, ignoreUser, ignoreRoles, ignoreGuild, memberRolesHaveCommandPermission, ignoreUserId } from './bonobotValidationUtils';
 import { Database } from './database';
 import { Id } from './idGenerator';
-import { MongoDbAdapter } from './mongoDbAdapter';
+import { mongoDbAdapter } from './mongoDbAdapter';
 import { expressCallback } from './expressCallback';
 import { authenticationMiddleware, bonobotMastersMiddleware, authorizationMiddleware } from './UserMiddleware';
 import { booleanFromString, isValidColor, arraysEqual, stringlength, isNormalInteger } from './validationUtils';
-import { MongoDatabase } from './mongoDb';
 
-export { startHerokuHackRequest, isBot, ignoreUser, ignoreUserId, ignoreRoles, ignoreGuild, memberRolesHaveCommandPermission, Database, MongoDbAdapter, Id, expressCallback, authenticationMiddleware, authorizationMiddleware, bonobotMastersMiddleware, booleanFromString, isValidColor, arraysEqual, stringlength, isNormalInteger, MongoDatabase };
+export { startHerokuHackRequest, isBot, ignoreUser, ignoreUserId, ignoreRoles, ignoreGuild, memberRolesHaveCommandPermission, Database, mongoDbAdapter, Id, expressCallback, authenticationMiddleware, authorizationMiddleware, bonobotMastersMiddleware, booleanFromString, isValidColor, arraysEqual, stringlength, isNormalInteger };
